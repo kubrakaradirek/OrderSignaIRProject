90266f5a7ae8741ee267a6cc90e0cbf67bde3a57
kubrakaradirek <karadirekkubra96@gmail.com>
2024-05-13 20:22:08 +0300

    progress bar bitti

--- OrderSignaIRApi/Hubs/SignalRHub.cs
+++ OrderSignaIRApi/Hubs/SignalRHub.cs
@@ -1,15 +1,84 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using OrderSignaIR.BusinessLayer.Abstract;
 using OrderSignaIR.DataAccessLayer.Concrete;
 
 namespace OrderSignaIRApi.Hubs
 {
     public class SignalRHub : Hub
     {
-        OrderSignaIRContext context = new OrderSignaIRContext();
-        public async Task SendCategoryCount()
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+        private readonly IOrderService _orderService;
+        private readonly IMoneyCaseService _moneyCaseService;
+        private readonly IMenuTableService _menuTableService;
+
+        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService)
         {
-            var value=context.Categories.Count();
+            _categoryService = categoryService;
+            _productService = productService;
+            _orderService = orderService;
+            _moneyCaseService = moneyCaseService;
+            _menuTableService = menuTableService;
+        }
+
+        public async Task SendStatistic()
+        {
+            var value=_categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount",value);
+
+            var value2 = _productService.TProductCount();
+            await Clients.All.SendAsync("ReceiveProductCount", value2);
+
+            var value3 = _categoryService.TActiveCategoryCount();
+            await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+
+            var value4 = _categoryService.TPassiveCategoryCount();
+            await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
+
+            var value5 = _productService.TProductCountByCategoryNameHamburger();
+            await Clients.All.SendAsync("ReceiveProductCountByCategoryNameHamburger", value5);
+
+            var value6 = _productService.TProductCountByCategoryNameDrink();
+            await Clients.All.SendAsync("ReceiveProductCountByCategoryNameDrink", value6);
+
+            var value7 = _productService.TProductPriceAvg();
+            await Clients.All.SendAsync("ReceiveProductPriceAvg", value7);
+
+            var value8 = _productService.TProductNameByMaxPrice();
+            await Clients.All.SendAsync("ReceiveProductNameByMaxPrice", value8);
+
+            var value9 = _productService.TProductNameByMinPrice();
+            await Clients.All.SendAsync("ReceiveProductNameByMinPrice", value9);
+
+            var value10 = _productService.TProductAveragePriceByHamburger();
+            await Clients.All.SendAsync("ReceiveProductAveragePriceByHamburger", value10);
+
+            var value11 = _orderService.TTotalOrderCount();
+            await Clients.All.SendAsync("ReceiveTotalOrderCount", value11);
+
+            var value12 = _orderService.TActiveOrderCount();
+            await Clients.All.SendAsync("ReceiveActiveOrderCount", value12);
+
+            var value13 = _orderService.TLastOrderPrice();
+            await Clients.All.SendAsync("ReceiveLastOrderPrice", value13);
+
+            var value14 = _moneyCaseService.TTotalMoneyCaseAmount();
+            await Clients.All.SendAsync("ReceiveTotalMoneyCaseAmount", value14);
+
+            var value15 = _menuTableService.TMenuTableCount();
+            await Clients.All.SendAsync("ReceiveMenuTableCount", value15);
+
+        }
+        public async Task SendProgress()
+        {
+            var value = _moneyCaseService.TTotalMoneyCaseAmount();
+            await Clients.All.SendAsync("ReceiveTotalMoneyCaseAmount", value);
+
+            var value2 = _orderService.TActiveOrderCount();
+            await Clients.All.SendAsync("ReceiveActiveOrderCount", value2);
+
+            var value3 = _menuTableService.TMenuTableCount();
+            await Clients.All.SendAsync("ReceiveMenuTableCount", value3);
         }
     }
 }
---
+++ OrderSignaIRWebUI/Controllers/ProgressBarsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderSignaIRWebUI.Controllers
+{
+    public class ProgressBarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
